c6d4526716a63b464c301b37dfae07dcfc80e7a0
letniki <nikitaletuta12@gmai.com>
2024-08-26 21:13:44 +0300

    finish

--- src/components/Header/HeaderComponent.tsx
+++ src/components/Header/HeaderComponent.tsx
@@ -8,11 +8,12 @@ const HeaderComponent = () => {
 
     return (
         <nav className={styles.block}>
-            <Link className={styles.Link} to={'https://github.com/letniki'}><img src="" alt=""/></Link>
+            <Link className={styles.Link} to={'https://github.com/letniki'}><img className={styles.image} src="/img.png" alt=""/></Link>
             <Link className={styles.Link} to={'pokemon'}>Pokemons Page</Link>
             <Link className={styles.Link} to={'pokemon/search'}>Search Pokemon</Link>
             <Link className={styles.Link} to={'pokemon/favourite'}><div className={styles.heart}>&#10084;</div></Link>
-            <Link className={styles.Link} to={'https://github.com/letniki'}><img className={styles.image} src="/img.png" alt=""/></Link>
+            <Link className={styles.Link} to={'https://github.com/letniki'}><img className={styles.image} src="/img_2.png" alt=""/></Link>
+
         </nav>
     );
 };
